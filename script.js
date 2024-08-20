324e36e0e39c801dbbb85de78e289128fe093e72
unknown <murugeshwarimurugan4@gmail.com>
2024-08-20 14:47:52 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+alert('test');
+
+setTimeout(()=>document.getElementsByClassName('name')[0].innerHTML='change name',3000)
+
+setTimeout(()=>document.getElementsByClassName('name')[1].innerHTML='change name',8000)
+
+alert(6+6)
+
+
+console.log('output')
